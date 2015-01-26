4915fb454fffe3c8bcf8b1d6cef122ab7b57441e
shadowbq <shadowbq@gmail.com>
2015-01-26 01:19:24 -0500

    version bump, namespaced, make file test added

--- src/cxtracker.h
+++ src/cxtracker.h
@@ -31,7 +31,7 @@
 #endif /* __FreeBSD__ */
 
 /*  D E F I N E S  ************************************************************/
-#define VERSION                       "0.9.8.4"
+#define VERSION                       "0.9.9.0-shadowbq"
 #define TIMEOUT                       45
 #define BUCKET_SIZE                   65537
 #define SNAPLENGTH                    65535
